0a89cdb182ac0f8a052a7ea89721590be948f5a7
sagar <sagargurung1001@gmail.com>
2025-03-19 13:06:30 +0000

    refactor code

--- js/helper/constant.js
+++ js/helper/constant.js
@@ -1,3 +1,9 @@
 const mainUserPage = 'focus-flow-main';
 const mainQnAPage = 'focus-flow-qna';
 const mainResultPage = 'focus-flow-result';
+const subjectErrorEmpty = 'Subject name cannot be empty.';
+const subjectErrorLong = 'Subject name is too long.';
+const validationSubjectExceed = 'You can only select a maximum of 5 subjects.';
+
+// export used by js file not by html file
+export { subjectErrorEmpty, subjectErrorLong, validationSubjectExceed };
