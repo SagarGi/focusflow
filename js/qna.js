827ae2694f1ad2002f60bbc67cc34b1c879e1a9d
sagar <sagargurung1001@gmail.com>
2025-03-28 23:01:39 +0000

    Added iterative logic

--- js/qna.js
+++ js/qna.js
@@ -3,7 +3,8 @@ import { getSeletedSubjects } from '../js/helper/helper.js';
 let choosenSubjects = getSeletedSubjects();
 const totalNoOfChoosenSubjects = choosenSubjects.length;
 let subjectVsQuestionCountForResult = {};
-let noOfTimesQuestionAsked = 0;
+let subjectIteratorStart = 0;
+let subjectIteratorEnd = subjectIteratorStart + 1;
 
 export function displaySelectedSubjects() {
   let subjects = getSeletedSubjects();
@@ -28,8 +29,45 @@ export function displaySelectedSubjects() {
   }
 }
 
-export function selectQuestionsSubject() {
-  // here we iterate the selected subjects and get the questions count for each subject
+export function selectQuestionsSubject(node) {
+  if (subjectIteratorStart > totalNoOfChoosenSubjects - 2) {
+    // at this point all subjects are selected
+    console.log(subjectVsQuestionCountForResult);
+    return;
+  }
+  const hardSubjectChoosen = node.innerText;
+  if (subjectVsQuestionCountForResult[hardSubjectChoosen]) {
+    subjectVsQuestionCountForResult[hardSubjectChoosen]++;
+  } else {
+    subjectVsQuestionCountForResult[hardSubjectChoosen] = 1;
+  }
+
+  // change the subject selection and also the subject names in the question bar
+  let selectedSubjectWrapper = document.querySelectorAll('.selected-subjects-qna button');
+  // remove the highlight from the previous subject
+  selectedSubjectWrapper[subjectIteratorEnd].classList.remove('selected');
+  subjectIteratorEnd++;
+  if (subjectIteratorEnd > totalNoOfChoosenSubjects - 1) {
+    // remove the highlight from the previous subject
+    selectedSubjectWrapper[subjectIteratorStart].classList.remove('selected');
+    subjectIteratorStart++;
+    if (subjectIteratorStart > totalNoOfChoosenSubjects - 2) {
+      // at this point all subjects are selected
+      console.log(subjectVsQuestionCountForResult);
+      document.querySelector('#firstOption').disabled = true;
+      document.querySelector('#secondOption').disabled = true;
+      document.querySelector('.result-button').style.display = 'flex';
+      return;
+    }
+    selectedSubjectWrapper[subjectIteratorStart].className = 'selected';
+    subjectIteratorEnd = subjectIteratorStart + 1;
+  }
+  // add highlight to the new subject
+  selectedSubjectWrapper[subjectIteratorEnd].className = 'selected';
+  document.querySelector('#firstOption').innerText =
+    selectedSubjectWrapper[subjectIteratorStart].innerText;
+  document.querySelector('#secondOption').innerText =
+    selectedSubjectWrapper[subjectIteratorEnd].innerText;
 }
 
 // This function is loaded only after the DOM is loaded
